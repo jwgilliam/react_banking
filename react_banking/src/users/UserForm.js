43d94f566edcf8deadc89c9f07442ecec341dd0e
jwgilliam <ggsmoustache@gmail.com>
2023-11-11 04:18:34 -0600

    func: added user form

---
+++ react_banking/src/users/UserForm.js
@@ -0,0 +1,94 @@
+import "./UserForm.css"
+import { useContext, useEffect } from "react"
+import { UserContext } from "./UserProvider"
+import { useState } from "react"
+
+export default function UserForm() {
+
+
+
+  const [isSignup, setIsSignup] = useState(false)
+
+
+
+
+  let signUpField = <><input type="email" className="user_email" required placeholder="e-mail"></input> <button className="register_button">Register</button></>
+
+  const { users, loginUser, registerUser, isSignedIn, setSignedIn } = useContext(UserContext)
+  useEffect(() => {
+    if (localStorage.getItem("activeUser")) {
+      setSignedIn(true)
+    }
+  }, [])
+  const loginHandler = (event) => {
+    event.preventDefault()
+
+    let formData = getFormData()
+
+    let loggedIn = localStorage.getItem("activeUser")
+
+    // console.log(isSignedIn)
+
+
+
+    const foundUser = users.find(registeredUser => registeredUser.userName === formData.userName && registeredUser.password === formData.password)
+    if (foundUser) {
+      loginUser(foundUser)
+      clearForm()
+      setSignedIn(true)
+    }
+
+    if (isSignup) {
+      registerUser(formData)
+      clearForm()
+      setIsSignup(false)
+
+      return
+    }
+    // console.log(foundUser)
+  }
+
+  const registrationHandler = () => {
+    setIsSignup(true)
+  }
+
+  const logoutHandler = () => {
+    localStorage.removeItem("activeUser")
+    setSignedIn(false)
+  }
+
+  const clearForm = () => {
+    document.querySelector(".user_name").value = ""
+    document.querySelector(".user_password").value = ""
+    if (isSignup) {
+      document.querySelector(".user_email").value = ""
+    }
+  }
+
+  const getFormData = () => {
+    let email
+    if (isSignup) {
+      email = document.querySelector(".user_email").value
+    }
+    const form = {
+      userName: document.querySelector(".user_name").value,
+      password: document.querySelector(".user_password").value,
+      email: email
+    }
+    return form
+  }
+
+  return (
+    <>
+      <form className="user_login" id="user_login_form" onSubmit={loginHandler}>
+        <input type="hidden"></input>
+        <input className="user_name" type="text" placeholder="user name" required></input>
+        <input className="user_password" type="password" placeholder="password" required></input>
+        {isSignup ? signUpField : null}
+
+      </form>
+      {isSignup ? null : <button className="register_button" onClick={registrationHandler}>Sign Up!</button>}
+      {isSignedIn ? <button className="logout_button" onClick={logoutHandler}> log out</button> : <button className="login_button" type="submit" form="user_login_form">Login</button>}
+    </>
+  )
+}
